08da8579bbdb714238a13a89a82f69c91d8b7d50
Pina <mejnour@pop-os.localdomain>
2020-10-05 00:05:39 -0300

    Adicionado leitor de arquivos.

--- lexico/src/main.rs
+++ lexico/src/main.rs
@@ -1,3 +1,16 @@
+fn le_arquivo() -> String {
+    let mut file = File::open(PATH)
+        .expect("Não foi possível abrir o arquivo.");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Não foi possível ler o arquivo.");
+
+    return contents;
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let programa_input: String = le_arquivo();
+    analisador_lexico(programa_input);
 }
