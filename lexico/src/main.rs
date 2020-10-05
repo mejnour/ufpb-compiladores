d1e4bc3b23fac11731daf4c7bd516198dc86e917
Pina <mejnour@pop-os.localdomain>
2020-10-05 00:15:54 -0300

    Adicionado caminhos dos arquivos de teste.

--- lexico/src/main.rs
+++ lexico/src/main.rs
@@ -20,6 +20,24 @@ const NUMEROS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const ALFABETO: [char; 52] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const PALAVRAS_RESERVADAS: [&str; 12] = ["program", "var", "procedure", "function", "begin", "end", "if", "then", "else", "while", "do", "not"];
 
+/* 
+
+Declarações dos caminhos dos arquivos de teste. 
+
+*/
+
+const PATH: &str = "src/benchmark-arquivos_testes/Test1.pas";
+// const PATH: &str = "src/benchmark-arquivos_testes/Test2.pas";
+// const PATH: &str = "src/benchmark-arquivos_testes/Test3.pas";
+// const PATH: &str = "src/benchmark-arquivos_testes/Test4.pas";
+// const PATH: &str = "src/benchmark-arquivos_testes/Test5.pas";
+
+/* 
+
+Leitor de arquivos de texto. 
+
+*/
+
 fn le_arquivo() -> String {
     let mut file = File::open(PATH)
         .expect("Não foi possível abrir o arquivo.");
@@ -34,5 +52,5 @@ fn le_arquivo() -> String {
 fn main() {
 
     let programa_input: String = le_arquivo();
-    
+
 }
